3b2a32556c60c047465306884c221b63bee55fbb
ericaohashi <m.0084erica.m.july@gmail.com>
2022-03-02 18:06:38 +0900

    ログインユーザー以外の表示を変更

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:create, :edit, :update,:destroy]
   def index
     @prototypes = Prototype.all
   end
