e3f02b0e091ef39272275d48e046fde40f342018
ericaohashi <m.0084erica.m.july@gmail.com>
2022-03-02 16:41:57 +0900

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.new(comment_params)
+    # @prototype = @comment.prototype
+    if @comment.save
+      redirect_to "/prototypes/#{@comment.prototype}"  
+    else
+      @prototype = @comment.prototype
+      render "prototypes/show"
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+    
+    # paramsの中にあるprototype_idをとる
+  end
+
+end
